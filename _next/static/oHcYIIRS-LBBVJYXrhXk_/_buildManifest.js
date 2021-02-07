15b21ec13d4bd866ab12657a281377f479bebe8a
sk62793 <sk62793@users.noreply.github.com>
2021-02-07 19:25:29 +0000

    deploy: 1a22989453ccb7db619540cc827cb4c857d47a11

---
+++ _next/static/oHcYIIRS-LBBVJYXrhXk_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-9b1e7e38078ddd2ae665.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a81f2db3930dfb09532b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
